fe39aace8cfc2caca4617cf0155e0fc4439ab42a
moon <92293235+tiffmoon@users.noreply.github.com>
2022-02-17 19:09:05 +1100

    everything workds

--- index.js
+++ index.js
@@ -1,128 +1,156 @@
-const inquirer = require('inquirer');
-const Employee = require('./lib/Employee');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const { prompt } = require('inquirer');
-
+const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const fs = require("fs");
+const { prompt } = require("inquirer");
+const generateHTML = require("./src/generatehtml");
 
 const questions = () => {
   return inquirer.prompt([
     {
-      type: 'list',
-      message: 'What is your role?',
-      name: 'role',
-      choices: ['Manager', 'Engineer', 'Intern', 'Employee'],
+      type: "list",
+      message: "What is your role?",
+      name: "role",
+      choices: ["Manager", "Engineer", "Intern", "Employee"],
     },
     {
-      type: 'input',
+      type: "input",
       message: `What is your name?`,
-      name: 'name',
+      name: "name",
       validate(answer) {
-        if(!answer) {
-            return "Pretty please, fill your name!";
+        if (!answer) {
+          return "Pretty please, fill your name!";
         }
         return true;
-      } 
+      },
     },
     {
-      type: 'input',
+      type: "input",
       message: `What is your id?`,
-      name: 'id',
+      name: "id",
       validate(answer) {
-        if(isNaN(answer)) {
+        if (isNaN(answer)) {
           return "Pretty please, fill your ID!";
-      }
-      return true;
-    } 
-    },
-    {
-      type: 'input',
-      message: `What is your email?`,
-      name: 'email',
-      validate(answer) {
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-        if(!emailRegex.test(answer)) {
-            return "Pretty please, provide a valid email address!"
         }
-        return true
-    }
+        return true;
+      },
     },
     {
-      type: 'input',
+      type: "input",
+      message: `What is your email?`,
+      name: "email",
+      validate(answer) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(answer)) {
+          return "Pretty please, provide a valid email address!";
+        }
+        return true;
+      },
+    },
+    {
+      type: "input",
       message: `What is your office number?`,
-      name: 'officeNumber',
+      name: "officeNumber",
       when: (input) => input.role === "Manager",
       validate(answer) {
-        if(isNaN(answer)) {
-            return "Pretty please, fill your office number!";
+        if (isNaN(answer)) {
+          return "Pretty please, fill your office number!";
         }
         return true;
-      }
+      },
     },
     {
-      type: 'input',
+      type: "input",
       message: `What is your github username?`,
-      name: 'github',
+      name: "github",
       when: (input) => input.role === "Engineer",
       validate(answer) {
-        if(!answer) {
-            return "Pretty please, fill your github username!";
+        if (!answer) {
+          return "Pretty please, fill your github username!";
         }
         return true;
-      }
+      },
     },
     {
-      type: 'input',
+      type: "input",
       message: `What is your school?`,
-      name: 'school',
+      name: "school",
       when: (input) => input.role === "Intern",
       validate(answer) {
-        if(!answer) {
-            return "Pretty please, fill your school!";
+        if (!answer) {
+          return "Pretty please, fill your school!";
         }
         return true;
-      }
+      },
     },
-    {
-      type: 'confirm',
-      message: 'Do you want to add another team member?',
-      name: 'addMember',
-    },
+    
   ]);
 };
 
-// use GetName function in markdown 
-// var maneger = new manager ()
-// with the answers
-// manager(answers.name, answers.)
+// How do i get the questions to repeat again if they selecte Y to adding a new team member?
 
-// need to know in advanced what it is - call new Manager
-// switch block based on type 
+const employees = [];
 
-async function init() {
-  const employees = [];
+async function askForNewEmployee() {
+  var answers = await questions();
 
-  var answers = await questions()
-  console.log(answers)
+  console.log(answers);
 
   switch (answers.role) {
-    case "Manager" :
-      employees.push(new Manager (answers.name, answers.id, answers.email, answers.officeNumber));
+    case "Manager":
+      employees.push(
+        new Manager(
+          answers.name,
+          answers.id,
+          answers.email,
+          answers.officeNumber
+        )
+      );
       break;
 
-    case "Engineer" :
-      employees.push(new Engineer (answers.name, answers.id, answers.email, answers.github));
+    case "Engineer":
+      employees.push(
+        new Engineer(answers.name, answers.id, answers.email, answers.github)
+      );
       break;
-    
-    case "Intern" :
-      employees.push(new Intern (answers.name, answers.id, answers.email, answers.school));
+
+    case "Intern":
+      employees.push(
+        new Intern(answers.name, answers.id, answers.email, answers.school)
+      );
       break;
+    // console.log(employees);
   }
- 
-    // .then((answers) => fs.writeFileSync('index.html', generateMarkdown(answers)))
-    // .then(() => console.log('Successfully created README.md!'))
-    // .catch((err) => console.error(err));
+  // ask the qustions again depeding if user wanted to add new employee
+  const wantsToAddNewMember = await  inquirer.prompt([
+    {
+      type: "confirm",
+      message: "Do you want to add another team member?",
+      name: "addMember",
+    },
+  ]).then((ans) => {
+    return ans.addMember;
+  })
+  
+
+  if(wantsToAddNewMember){
+    await askForNewEmployee();
+  }
+
+
+}
+
+async function init() {
+  await askForNewEmployee();
+
+  console.log(employees);
+
+  fs.writeFileSync(__dirname + "/dist/index.html", generateHTML(employees)); // is this right??
+  // .then((answers) => fs.writeFileSync('index.html', generateMarkdown(answers)))
+  // .then(() => console.log('Successfully created README.md!'))
+  // .catch((err) => console.error(err));
 }
 
 // const promptA = await inquirer.prompt({
@@ -135,3 +163,11 @@ async function init() {
 // }
 
 init();
+
+// function
+
+// fs.writeFileSync('index.html', generateHTML(answers), (err) =>
+//       err ? console.log(err) : console.log('Successfully created index.html!')
+//     );
+
+// How do i create a css file as well? or can that aready be created?
---
+++ src/generateHTML.js
@@ -0,0 +1,84 @@
+function createEmployeeCard(employee){
+
+
+  const role = employee.getRole();
+
+  let specialItemKey, specialItemValue;
+  if(role === 'Manager'){
+    specialItemKey = 'Office Number';
+    specialItemValue = employee.getOfficeNumber();
+  }else if(role === 'Engineer'){
+
+  }
+  
+
+
+  return `
+  
+  <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${employee.getName()}</h5>
+      <h3 class ="card-subtitle mb-2 text-muted">${employee.getRole()}</h3>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${employee.getID()}</li>
+      <li class="list-group-item">Email: <a href="mailto:${employee.getEmail()}"> ${employee.getEmail()}</a>
+        </li>
+
+      <li class="list-group-item">${specialItemKey}: ${specialItemValue}</li>
+      
+      
+    </ul>
+  </div>
+`
+}
+
+
+
+function generateHTML(employees) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./dist/style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <title>Employees</title>
+  </head>
+  <header>
+    <h1>My Team</h1>
+  </header>
+  <body>
+  
+  <section class="d-flex justify-content-center">
+    ${employees.map((employee) => createEmployeeCard(employee) ).join("") }
+
+    </section>
+  </body>
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </html>`;
+}
+
+// how can i make the last list item change depending on if its manager, intern, engineer??
+// WHEN I click on an email address in the HTML
+// THEN my default email program opens and populates the TO field of the email with the address
+
+
+// switch (employees.role) {
+//   case "Manager" :
+//     getOfficeNumber();
+
+
+//   case "Engineer" :
+//     employees.push(new Engineer (answers.name, answers.id, answers.email, answers.github));
+//     break;
+  
+//   case "Intern" :
+//     employees.push(new Intern (answers.name, answers.id, answers.email, answers.school));
+//     break;
+// }
+
+module.exports = generateHTML;
